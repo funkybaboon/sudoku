8e318aff743b4fad81bb355a0a6912e11ed904cd
Benjamin Ißler <b.issler@mwaysolutions.com>
2015-10-11 18:51:48 +0200

    add end to end tests

---
+++ sudoku-spec.js
@@ -0,0 +1,10 @@
+describe('vanilla js sudoku', function() {
+    it('should open the start sudoku', function() {
+
+        browser.ignoreSynchronization = true;
+
+        browser.get('http://127.0.0.1:8080/');
+
+        element(by.id('tableCreate')).click();
+    });
+});
