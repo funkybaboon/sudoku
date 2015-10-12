7ec3701a151e6954311c89d44efd61b430e1d0e9
Benjamin Ißler <b.issler@mwaysolutions.com>
2015-10-12 19:13:14 +0200

    test count amount of blocks

--- sudoku-spec.js
+++ sudoku-spec.js
@@ -6,5 +6,8 @@ describe('vanilla js sudoku', function() {
         browser.get('http://127.0.0.1:8080/');
 
         element(by.id('tableCreate')).click();
+
+        var elements = element.all(protractor.By.css('.block'));
+        expect(elements.count()).toEqual(9);
     });
 });
