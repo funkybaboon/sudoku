88f253d74c3737d6129a0cdd124bd73be5896ae7
Benjamin Ißler <b.issler@mwaysolutions.com>
2015-10-14 00:14:58 +0200

    add check for first row of default sudoku board

--- sudoku-spec.js
+++ sudoku-spec.js
@@ -7,11 +7,36 @@ describe('vanilla js sudoku', function() {
 
         element(by.id('tableCreate')).click();
 
+        browser.sleep(1000);
+
         var blocks = element.all(protractor.By.css('.block'));
         expect(blocks.count()).toEqual(9);
 
         var cells = element.all(protractor.By.css('.cell'));
         expect(cells.count()).toEqual(81);
 
+        expect(element(by.id('x1y1')).getText()).toEqual('1');
+        expect(element(by.id('x9y1')).getText()).toEqual('3');
+        expect(element(by.id('x5y2')).getText()).toEqual('6');
+        expect(element(by.id('x3y3')).getText()).toEqual('3');
+        expect(element(by.id('x6y3')).getText()).toEqual('1');
+
+
+        /*
+         sudoku[2][4]['value'] = 7;
+         sudoku[4][4]['value'] = 1;
+         sudoku[3][5]['value'] = 8;
+         sudoku[7][5]['value'] = 5;
+         sudoku[6][6]['value'] = 3;
+         sudoku[8][6]['value'] = 4;
+
+         sudoku[4][7]['value'] = 8;
+         sudoku[7][7]['value'] = 6;
+         sudoku[5][8]['value'] = 1;
+         sudoku[1][9]['value'] = 6;
+         sudoku[9][9]['value'] = 7;
+         */
+
     });
+
 });
